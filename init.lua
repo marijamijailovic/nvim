fbdfdcf9aca8b6c4f7fdbc0f7bdb14af8096baf0
marijamijailovic <marija.mijailovic@mvpworkshop.co>
2024-05-19 17:47:15 +0200

    Update

--- init.lua
+++ init.lua
@@ -1,12 +1,2 @@
-require("mami.plugins-setup") -- Load Plugins Configuration
-require("mami.core.options") -- Load Neovim Basic Configuration
-require("mami.core.keymaps") -- Load custom keybindings for Neovim
-require("mami.core.colorscheme") -- Load custom color schema
-require("mami.plugins.comment") -- Load custom comment plugin setup
-require("mami.plugins.nvim-tree") -- Load custom nvim-tree plugin setup
-require("mami.plugins.lualine") -- Load custom lualine plugin setup
-require("mami.plugins.telescope") -- Load custom telescope plugin setup
-require("mami.plugins.nvim-cmp") -- Load custom nvim cmp plugin setup
-require("mami.plugins.lsp.mason") -- Load mason lsp plugin setup
-require("mami.plugins.lsp.lspsaga") -- Load lsp saga plugin setup
-require("mami.plugins.lsp.lspconfig") -- Load lsp config plugin setup
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,64 @@
+-- lsp configs
+return {
+
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- customize lsp configs
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        rust_analyzer = {
+          keys = {
+            { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+            { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+            { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+          },
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              -- Add clippy lints for Rust.
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
+              },
+            },
+          },
+        },
+        taplo = {
+          keys = {
+            {
+              "K",
+              function()
+                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+                  require("crates").show_popup()
+                else
+                  vim.lsp.buf.hover()
+                end
+              end,
+              desc = "Show Crate Documentation",
+            },
+          },
+        },
+      },
+    },
+  },
+  -- add circom
+  { "rubydusa/circom-lsp" },
+}
